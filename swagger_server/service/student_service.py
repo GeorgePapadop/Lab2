d0d240ca790f3b32c916f3387c9f21d963494bd3
george-papado <my e-mail>
2020-02-13 16:47:43 +0100

    student_service update

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -18,10 +18,8 @@ student_db = TinyDB(db_file_path)
 def add_student(student):
     queries = []
     query = Query()
-    if(student.first_name != None):
-        queries.append(query.first_name == student.first_name)
-    if(student.last_name != None):
-        queries.append(query.last_name == student.last_name)
+    queries.append(query.first_name == student.first_name)
+    queries.append(query.last_name == student.last_name)
     query = reduce(lambda a, b: a & b, queries)
     res = student_db.search(query)
     if res:
